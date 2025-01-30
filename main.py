3036d0f6240177b4dbd0550e3fd16830b131d2d3
Robert Guzman <101305148+Robjguzman@users.noreply.github.com>
2025-01-30 19:08:40 +0000

    first python training

---
+++ main.py
@@ -0,0 +1,22 @@
+# python 1
+# basic data types
+
+h = "hello"
+w = 'world'
+
+print(h,w)
+print(h + w)
+
+n1 = 5
+multistring = """
+a lot of test can be place here
+float()
+str()
+int()
+
+
+
+"""
+
+
+print(h+str(n1))
